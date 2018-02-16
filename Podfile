e7e1e0a09a353d742a9ad176f1627a182654edcc
frank.xu <frank.xu@touch-spring.com>
2018-02-16 12:46:34 +0800

    init

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '8.0'
+
+target 'YYText+List' do
+
+pod 'YYText'
+
+end
---
+++ YYText+List/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  YYText+List
+//
+//  Created by Xuzixiang on 2018/2/16.
+//  Copyright © 2018年 frankxzx. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
